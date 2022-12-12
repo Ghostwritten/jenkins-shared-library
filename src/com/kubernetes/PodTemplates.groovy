ecf9353831650f5b7a2a97b316281407fd7de149
ghostwritten <1zoxun1@gmail.com>
2022-12-12 22:56:11 +0800

    fix PodTemplate.groovy

--- src/com/kubernetes/PodTemplates.groovy
+++ src/com/kubernetes/PodTemplates.groovy
@@ -2,7 +2,7 @@ package com.ghostwritten.PodTemplates
 
 public void dockerTemplate(body) {
   podTemplate(
-        containers: [containerTemplate(name: 'docker', image: 'docker', command: 'sleep', args: '99d')],
+        containers: [containerTemplate(name: 'docker', image: 'docker:19.03.1', command: 'sleep', args: '99d')],
         volumes: [hostPathVolume(hostPath: '/var/run/docker.sock', mountPath: '/var/run/docker.sock')]) {
     body.call()
 }
@@ -10,7 +10,7 @@ public void dockerTemplate(body) {
 
 public void mavenTemplate(body) {
   podTemplate(
-        containers: [containerTemplate(name: 'maven', image: 'maven', command: 'sleep', args: '99d')]) {
+        containers: [containerTemplate(name: 'maven', image: 'maven:3.8.1-jdk-8', command: 'sleep', args: '99d')]) {
     body.call()
 }
 }
